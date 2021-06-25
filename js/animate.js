e9bc5663f2d79b5b1a4d8fd32bddd6cf747ecd48
Soumya Ranjan Patnaik <soumyaranjan1812@google.com>
2021-06-25 15:53:08 +0530

    Animation starts after pageload

---
+++ js/animate.js
@@ -0,0 +1,5 @@
+function animate(){
+	temp = document.body.innerHTML;
+	document.getElementById('sec-1').style.animation='splashanime 1s ease-in-out 1.8s 1 forwards';
+	document.getElementById('navlist').style.animation='navAnime 1.6s ease-in-out 0.2s 1 forwards';
+}
